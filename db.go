160e0a1e12f2c3af1f17f1b3e490bf20752701ac
CainChu <cainmaila@gmail.com>
2017-12-25 18:05:35 +0800

    刪除

--- db.go
+++ db.go
@@ -35,6 +35,21 @@ func main() {
 	}
 	fmt.Printf("新增了 %d\n", addKey)
 
+	//刪除
+	stmt, err = db.Prepare("DELETE FROM tb WHERE name=?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err = stmt.Exec("Tony3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	num, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("刪除了 %d 筆\n", num)
+
 	//查詢
 	// rows, err := db.Query("select id, name, age from tb where name = ?", "Cain")
 	rows, err := db.Query("select id, name, age from tb")
